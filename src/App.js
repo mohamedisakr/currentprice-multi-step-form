f48a6adbf21835fc39fb8f35b3e0a56bb8026ae5
mohamedisakr <mohamedisakr@gmail.com>
2022-01-10 12:00:21 +0200

    consuming api endpoint & display its info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 import './App.css'
 
-const ApiEndpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+const apiEndpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
-function App() {
+export const read = async () => {
+  try {
+    const res = await axios.get(`${apiEndpoint}`)
+    return res
+  } catch (err) {
+    console.log(`Error : ${err}`)
+    return err.response.data.error
+  }
+}
+
+const App = () => {
+  const [prices, setPrices] = useState({})
+
+  useEffect(() => {
+    loadPrices()
+  }, [])
+
+  const loadPrices = async () => {
+    const {data} = await read()
+    setPrices(data)
+    const {time, disclaimer, chartName, bpi} = data
+    const {USD, GBP, EUR} = bpi
+    console.log(bpi)
+  }
+
   return (
     <div className="parent">
+      <div>
+        <pre>{JSON.stringify(prices, null, 4)}</pre>
+      </div>
       <div className="child">
         <div className="grand-child">
           <p>grandchild</p>
