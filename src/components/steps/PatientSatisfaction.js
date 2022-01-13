cb3e0ae589568d66d9ef57249fae25720ef96271
mohamedisakr <mohamedisakr@gmail.com>
2022-01-13 14:23:12 +0200

    adding select control to satisfaction step

--- src/components/steps/PatientSatisfaction.js
+++ src/components/steps/PatientSatisfaction.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import {useState} from 'react'
+import {
+  Container,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+} from '@mui/material'
 
-const PatientSatisfaction = () => {
+// {
+//     name: '',
+//     selectedOption: '',
+//     amount: 0,
+//     retryNow: '',
+//     retryLater: '',
+//   }
+const PatientSatisfaction = ({formData, setFormData, navigation}) => {
+  let {selectedOption} = formData
+  const [selected, setSelected] = useState('')
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setSelected(event.target.value)
+    selectedOption = selected
+  }
+
+  console.log(`selected : ${selected}`)
+  console.log(`selectedOption : ${selectedOption}`)
+
   return (
-    <div>
-      <h1>Patient Satisfaction</h1>
-    </div>
+    <Container maxWidth="xs">
+      <h3>Patient Satisfaction</h3>
+      <Box sx={{minWidth: 120}}>
+        <FormControl fullWidth style={{marginTop: '1.5rem'}}>
+          <InputLabel id="select-label">Please choose one option</InputLabel>
+          <Select
+            labelId="select-label"
+            id="simple-select"
+            value={selected}
+            // label="Please choose one option"
+            onChange={handleChange}
+          >
+            <MenuItem value="satisfied">Satisfied</MenuItem>
+            <MenuItem value="unsatisfied">Unsatisfied</MenuItem>
+            <MenuItem value="indifferent">Indifferent</MenuItem>
+          </Select>
+          {}
+        </FormControl>
+      </Box>
+    </Container>
   )
 }
 
